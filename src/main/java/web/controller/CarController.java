7f1b148cc9f380e898ee8f9f1022e668108fddbe
Pavel Ripanov <shurf.pc@gmail.com>
2024-12-08 13:44:48 +0300

    Autowired carService

--- src/main/java/web/controller/CarController.java
+++ src/main/java/web/controller/CarController.java
@@ -1,15 +1,16 @@
 package web.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import web.Service.CarService;
-import web.Service.CarServiceImp;
 
 @Controller
 public class CarController {
-    CarService carService = new CarServiceImp();
+    @Autowired
+    CarService carService;
 
     @GetMapping("/cars")
     public String cars(@RequestParam(value = "count", defaultValue = "5") Integer count, Model model) {
